f2afc9158c852b5febfe9f9fef4909980fce5b0f
Sailesh-2209 <saileshr2209@gmail.com>
2021-01-25 21:44:13 +0530

    exercise - 6

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # [Introduction to the Basic Node and Express Challenges](https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/)
+
+**This app is hosted on heroku at - "https://fcc-mircroservices.herokuapp.com/"**
--- myApp.js
+++ myApp.js
@@ -7,14 +7,20 @@ console.log("Hello World");
 //   res.send("Hello Express");
 // });
 
+process.env.VAR_NAME = uppercase;
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.use("/", express.static(__dirname + "/public"));
 app.use("/json", (req, res) => {
+  let message = "Hello json";
+  if (process.env.VAR_NAME === uppercase) {
+    message = "HELLO JSON";
+  }
   res.json({
-    message: "Hello json",
+    message: message,
   });
 });
 
