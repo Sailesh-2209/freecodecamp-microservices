1d8095f1b6fc720117a38186933b6afd5dfd8109
Sailesh-2209 <saileshr2209@gmail.com>
2021-03-29 20:47:29 +0530

    exercise 7

--- myApp.js
+++ myApp.js
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 app.use("/", express.static(__dirname + "/public"));
 app.use("/json", (req, res) => {
   if (process.env.MESSAGE_STYLE === "uppercase") {
